2167c9f8308a74dcc4081a96ba0b79f95db2e96b
Guilherme Chaves Volpato <guilherme.94720@alunosatc.edu.br>
2023-09-11 21:33:44 -0300

    aula 6 finalizada

--- src/main/java/com/satc/satcloja/repository/ServicoRepository.java
+++ src/main/java/com/satc/satcloja/repository/ServicoRepository.java
@@ -1,11 +1,9 @@
 package com.satc.satcloja.repository;
 
-import com.satc.satcloja.model.Produto;
+import com.satc.satcloja.model.Servico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+public interface ServicoRepository extends JpaRepository<Servico, Long>{
 }
-
-
